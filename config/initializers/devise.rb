07fe9c4911bcdec76597082a9d323af05d255fa9
leon.dahmer <leon.dahmer@gmail.com>
2019-04-01 04:57:01 -0400

    Share

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@the-radr.com'
+  config.mailer_sender = 'noreply@the-radr.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
