227c35c7b31f10d930113dcd01136afd69bb901c
leon.dahmer <leon.dahmer@gmail.com>
2019-04-08 05:56:34 -0400

    Invoice Error

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,29 +4,29 @@ class Invoice < ApplicationRecord
   def date_str
     case self.date.month
       when 1 
-        return "Januar" + " " + self.created_at.year 
+        return "Januar" + " " + self.created_at.year.to_s 
       when 2
-        return "Februar" + " " + self.created_at.year
+        return "Februar" + " " + self.created_at.year.to_s
       when 3
-        return "März" + " " + self.created_at.year
+        return "März" + " " + self.created_at.year.to_s
       when 4
-        return "April" + " " + self.created_at.year
+        return "April" + " " + self.created_at.year.to_s
       when 5
-        return "Mai" + " " + self.created_at.year
+        return "Mai" + " " + self.created_at.year.to_s
       when 6
-        return "Juni" + " " + self.created_at.year
+        return "Juni" + " " + self.created_at.year.to_s
       when 7 
-        return "July" + " " + self.created_at.year
+        return "July" + " " + self.created_at.year.to_s
       when 8
-        return "August" + " " + self.created_at.year
+        return "August" + " " + self.created_at.year.to_s
       when 9
-        return "September" + " " + self.created_at.year
+        return "September" + " " + self.created_at.year.to_s
       when 10
-        return "Oktober" + " " + self.created_at.year
+        return "Oktober" + " " + self.created_at.year.to_s
       when 11
-        return "November" + " " + self.created_at.year
+        return "November" + " " + self.created_at.year.to_s
       when 12 
-        return "Dezember" + " " + self.created_at.year
+        return "Dezember" + " " + self.created_at.year.to_s
     end
       
   end
